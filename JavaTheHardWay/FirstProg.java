b51a7a4f737a1d649d8f01452f819de9b4322935
Adam <adamonwan@gmail.com>
2019-08-31 22:50:36 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg {
+
+  public static void main(String[] args) {
+    System.out.println("I, Adam Onwan, am determined to learn how to code.");
+    System.out.println("Today's date is August 31, 2019.");
+  }
+
+}
