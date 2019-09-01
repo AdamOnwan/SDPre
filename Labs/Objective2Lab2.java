58c9696fad04fe690cbc6d8d04657bec91c71db4
Adam <adamonwan@gmail.com>
2019-09-01 02:07:09 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Adam Onwan";
+    int age = 30;
+    String state = " North Dakota";
+    System.out.println( "Name: " + name + " Age: " + age + " State: " + state );
+  }
+}
