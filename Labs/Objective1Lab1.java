29ecbbdb502ae5b97ed50638a12751de516aa9dd
Adam <adamonwan@gmail.com>
2019-09-01 00:36:32 -0600

    complete objective1lab1

---
+++ Labs/Objective1Lab1.java
@@ -0,0 +1,10 @@
+public class Objective1Lab1{
+  public static void main(String[] args){
+
+    /*
+      Your solution goes here
+    */
+    System.out.println( "Adam Onwan" ); //It says Expected Output Adam Onwan hence did not type solution in that area
+
+  }
+}
