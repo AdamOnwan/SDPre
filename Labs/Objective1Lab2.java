3897e9b50a39e5b58a942eb87db8ca50e667faf6
Adam <adamonwan@gmail.com>
2019-09-01 00:47:45 -0600

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2 {
+  public static void main( String[] args) {
+    //I'm assuming I don't have to type the comments provided
+    System.out.println( "Adam Onwan\n27" );
+  }
+}
