798e4dc3371d4c25932d62ab7b8469bda8b83d0c
Adam <adamonwan@gmail.com>
2019-09-02 17:25:29 -0600

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    int count, sum;
+
+    count = 0;
+    sum = 0 + count;
+
+      while(count <= 20) {
+        count = count + 1;
+        sum = count + sum;
+        System.out.println(sum);
+
+      }
+  }
+}
