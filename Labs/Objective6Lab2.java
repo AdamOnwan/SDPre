1569882c01955b504127d34f46eb57f1119e324d
Adam <adamonwan@gmail.com>
2019-09-01 23:13:38 -0600

    complete objective6lab2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    int counter = 0;
+
+      while(counter <= 20) {
+        System.out.println(counter);
+        counter = counter + 1;
+
+      }
+  }
+}
