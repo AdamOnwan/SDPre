33a2d2ad9d5359a32ee0784dba28e207c8ad85d9
Adam <adamonwan@gmail.com>
2019-09-02 21:06:06 -0600

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+
+  printName();
+    /*
+    Your solution goes here
+    */
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
