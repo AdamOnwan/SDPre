0afb6e26d2684e6874b63a6a6cfe01dcfaf4ce62
Adam <adamonwan@gmail.com>
2019-09-05 16:53:51 -0600

    cp ex 1

---
+++ JavaTheHardWay/aaFirstProg.java
@@ -0,0 +1,6 @@
+public class aaFirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "Adam is determined to learn how to code." );
+    System.out.println( "Today's date is Sep 5, 2019" );
+  }
+}
